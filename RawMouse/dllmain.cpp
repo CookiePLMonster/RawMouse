51aad0ffb3df92aa1b777f22bda580971f837571
Silent <zdanio95@gmail.com>
2016-10-16 15:09:03 +0200

    Correctly poll available buttons on the mouse
    Improvements to read safety (ensure sizes are OK before reading RI data)

--- RawMouse/dllmain.cpp
+++ RawMouse/dllmain.cpp
@@ -53,7 +53,7 @@ public:
 	float	Y;
 
 public:
-	inline bool		CheckForInput()
+	inline bool		CheckForInput() const
 		{ return lmb || rmb || mmb || wheelUp || wheelDown || bmx1 || bmx2 || X != 0.0f || Y != 0.0f; }
 };
 
@@ -62,6 +62,9 @@ class CMousePointerStateHelper
 public:
 	bool				m_bVerticalInvert;
 	bool				m_bHorizontalInvert;
+
+public:
+	CMouseControllerState*	GetMouseSetUp( CMouseControllerState* mouseCaps );
 };
 
 class CControllerState
@@ -163,9 +166,50 @@ CMenuManager&					FrontEndMenuManager = **AddressByVersion<CMenuManager**>(0x405
 
 RsGlobalType&	RsGlobal = **AddressByVersion<RsGlobalType**>(0x619604, 0, 0);
 
+int&	snTimeInMilliseconds =  **AddressByVersion<int**>(0x53F51C, 0, 0);
+
 inline bool IsForeground()
 {
 	return *(BOOL*)0x8D621C != FALSE;
+}
+
+CMouseControllerState* CMousePointerStateHelper::GetMouseSetUp( CMouseControllerState* mouseCaps )
+{
+	mouseCaps->lmb = false;
+	mouseCaps->rmb = false;
+	mouseCaps->mmb = false;
+	mouseCaps->wheelUp = false;
+	mouseCaps->wheelDown = false;
+	mouseCaps->X = mouseCaps->Y = mouseCaps->Z = 0.0f;
+	mouseCaps->bmx1 = false;
+	mouseCaps->bmx2 = false;
+
+	UINT numRIDevices = 0;
+	GetRawInputDeviceList( nullptr, &numRIDevices, sizeof(RAWINPUTDEVICELIST) );
+	if ( numRIDevices > 0 )
+	{
+		PRAWINPUTDEVICELIST pDevices = new RAWINPUTDEVICELIST[ numRIDevices ];
+		GetRawInputDeviceList( pDevices, &numRIDevices, sizeof(RAWINPUTDEVICELIST) );
+		for( size_t i = 0; i < numRIDevices; ++i )
+		{
+			if ( pDevices[i].dwType == RIM_TYPEMOUSE )
+			{	
+				RID_DEVICE_INFO deviceInfo;
+				UINT cbSize = sizeof(deviceInfo);
+				GetRawInputDeviceInfo( pDevices[i].hDevice, RIDI_DEVICEINFO, &deviceInfo, &cbSize );
+
+				if ( !mouseCaps->lmb ) mouseCaps->lmb = deviceInfo.mouse.dwNumberOfButtons >= 1;
+				if ( !mouseCaps->rmb ) mouseCaps->rmb = deviceInfo.mouse.dwNumberOfButtons >= 2;
+				// Assume MMB is scroll - seems like there's no obvious way to check for the presence of scroll wheel so this has to suffice
+				if ( !mouseCaps->mmb ) mouseCaps->mmb = mouseCaps->wheelDown = mouseCaps->wheelUp = deviceInfo.mouse.dwNumberOfButtons >= 3;
+				if ( !mouseCaps->bmx1 ) mouseCaps->bmx1 = deviceInfo.mouse.dwNumberOfButtons >= 4;
+				if ( !mouseCaps->bmx2 ) mouseCaps->bmx2 = deviceInfo.mouse.dwNumberOfButtons >= 5;
+			}
+		}
+		delete[] pDevices;
+	}
+
+	return mouseCaps;
 }
 
 void CPad::UpdateMouse()
@@ -188,8 +232,8 @@ void CPad::UpdateMouse()
 		PCTempMouseControllerState.X = PCTempMouseControllerState.Y = PCTempMouseControllerState.Z = 0.0f;
 		PCTempMouseControllerState.wheelDown = PCTempMouseControllerState.wheelUp = false;
 		
-		//if ( NewMouseControllerState.CheckForInput() )
-		//	LastTimeTouched = CTimer::m_snTimeInMilliseconds;
+		if ( NewMouseControllerState.CheckForInput() )
+			LastTimeTouched = snTimeInMilliseconds;
 	}
 }
 
@@ -248,9 +292,9 @@ void RegisterMouseMovement(RAWINPUT* raw)
 	if ( raw->data.mouse.usButtonFlags & RI_MOUSE_WHEEL )
 	{
 		StateBuf.Z += static_cast<signed short>(raw->data.mouse.usButtonData);
-		if ( StateBuf.Z < 0.0 )
+		if ( StateBuf.Z < 0.0f )
 			StateBuf.wheelDown = true;
-		else if ( StateBuf.Z > 0.0 )
+		else if ( StateBuf.Z > 0.0f )
 			StateBuf.wheelUp = true;
 	}
 }
@@ -262,14 +306,23 @@ LRESULT CALLBACK CustomWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
 	{
 	case WM_INPUT:
 		{
-			RAWINPUT	raw;
-			UINT		dwSize = sizeof(raw);
+			UINT dwSize = 0;
+			static UINT dwSavedSize = 0;
+			static BYTE* pRawBuf = nullptr;
 
-			GetRawInputData(reinterpret_cast<HRAWINPUT>(lParam), RID_INPUT,  &raw, &dwSize, sizeof(RAWINPUTHEADER));
-    
-			if ( raw.header.dwType == RIM_TYPEMOUSE )
+			GetRawInputData(reinterpret_cast<HRAWINPUT>(lParam), RID_INPUT,  nullptr, &dwSize, sizeof(RAWINPUTHEADER));
+			if ( dwSize > dwSavedSize )
 			{
-				RegisterMouseMovement(&raw);
+				delete[] pRawBuf;
+				pRawBuf = new BYTE[dwSize];
+				dwSavedSize = dwSize;
+			}
+    
+			RAWINPUT* raw = reinterpret_cast<RAWINPUT*>(pRawBuf);
+			GetRawInputData(reinterpret_cast<HRAWINPUT>(lParam), RID_INPUT,  raw, &dwSize, sizeof(RAWINPUTHEADER));
+			if ( raw->header.dwType == RIM_TYPEMOUSE )
+			{
+				RegisterMouseMovement(raw);
 			}
 			break;
 		}
@@ -287,7 +340,9 @@ void Patch_SA_10()
 
 	InjectHook(0x74880B, RegisterRawInputMouse);
 	Patch<BYTE>(0x7469A0, 0xC3);
-	Patch<DWORD>(0x53F2D0, 0x900004C2);
+	
+	// Mouse caps
+	InjectHook( 0x53F2D0, &CMousePointerStateHelper::GetMouseSetUp, PATCH_JUMP );
 
 	OldWndProc = *(LRESULT (CALLBACK***)(HWND, UINT, WPARAM, LPARAM))AddressByRegion_10<DWORD>(0x748454);
 	Patch(AddressByRegion_10<DWORD>(0x748454), &pCustomWndProc);
